c30c51ebead6cb4f4fdecab5768f613ab6a9eee5
nforla <nicoforlano@protonmail.com>
2019-11-10 15:09:53 -0300

    Commit inicial

---
+++ src/test/java/tap/nforla/microservicioautenticacion/MicroservicioAutenticacionApplicationTests.java
@@ -0,0 +1,13 @@
+package tap.nforla.microservicioautenticacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioAutenticacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
